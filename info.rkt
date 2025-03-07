63429180efefb53fcb39508f81dbe9aa476271cc
Ryan Wilcox <rwilcox@wilcoxd.com>
2025-03-07 14:47:22 -0500

    did not know about needing files list

--- info.rkt
+++ info.rkt
@@ -1,8 +1,9 @@
 #lang setup/infotab
 (define name "scribble_gen")
-(define version "1.0")
+(define version "1.0.1")
 (define deps '("base"
                "scribble-lib"
                "commonmark-lib"))
+(define files (list "scribble_gen.rkt" "helpers.rkt"))
 (define racket-launcher-names '("scribble_gen"))
 (define racket-launcher-libraries '("scribble_gen.rkt"))
